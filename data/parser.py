26a8b9237c4154faa3dd49c42c50d8154ed17a95
Mattia Dal Ben <matthewdibi@gmail.com>
2019-07-17 22:08:50 +0200

    Improved parser
    Removed columns with null standard deviation
    min-max normalization

--- data/parser.py
+++ data/parser.py
@@ -1,8 +1,24 @@
 import pandas as pd
+import numpy as np
 
-raw_data_path = '/home/mattia/Desktop/ProgettoSteve/ToyDataset/Validation/part-009.csv'
+raw_data_path = 'part-009.csv'
 
-data = pd.read_csv(raw_data_path, usecols=range(5,235))
+raw_data = pd.read_csv(raw_data_path, usecols=range(4, 235))
 
-print("Original data: ", data.shape)
-print(data.head())
+print("Original data: ", raw_data.shape)
+# print(raw_data.head())
+
+# delete columns with null standard deviation
+null_std = (raw_data.std(axis=0) == 0)
+data = raw_data.loc[:, raw_data.std() != 0.0]
+
+# min-max normalization
+data = np.array(data, dtype=np.float64)
+sensor_n = data.shape[1]
+
+max_value = np.max(data, axis=0)
+min_value = np.min(data, axis=0)
+
+data = (data - min_value)/(max_value - min_value + 1e-6)
+
+print(data.shape)
